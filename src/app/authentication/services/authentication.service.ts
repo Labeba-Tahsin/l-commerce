50303b36ec24b6d48462cf9fdc1c3e0a44accfd0
Labeba-Tahsin <labeba.tahsin@bs-23.net>
2022-06-09 13:08:02 +0600

    Auth services

--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  login({ email, password }: any): Observable<any> {
+    if (email === 'admin@gmail.com' && password === 'admin123') {
+      this.setToken('abcdefghijklmnopqrstuvwxyz');
+      return of({ name: 'Tarique Akhtar', email: 'admin@gmail.com' });
+    }
+    return throwError(() => new Error('Failed to login'));
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
+  }
+
+  isLoggedIn() {
+    return this.getToken() !== null;
+  }
+
 }
